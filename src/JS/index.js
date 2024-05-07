ac48168e0ef5c4f496b6669573224bf7e8d81ffa
Naomy25sandi <naomysandi25@gmail.com>
2024-05-07 15:42:54 -0600

    agregue funciones a mi js

--- src/JS/index.js
+++ src/JS/index.js
@@ -3,5 +3,20 @@ const email = document.getElementById("email")
 const password = document.getElementById("password")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
+const btnRegistrarse = document.getElementById("btnRegistrarse")
 
+btnRegistrarse.addEventListener("click",()=>{
+    localStorage.setItem("nombre",nombre.value)
+    localStorage.setItem("password",password.value)
+    let nombreMostrar = localStorage.getItem("nombre")
+    let claveMostrar = localStorage.getItem("password")
+    alert(nombreMostrar)
+    alert(claveMostrar)
+})
 
+email.addEventListener("click",()=>{
+    localStorage.setItem("email",email.value)
+})
+password.addEventListener("click",()=>{
+    localStorage.setItem("password",password.value)
+})
---
+++ src/JS/login.js
@@ -0,0 +1,14 @@
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", (e) => {
+  if (email.value === "naomysandi25@gmail.com" && password.value === "251087nsq") {
+    e.preventDefault()
+    window.location.href="portafolio.html"
+  } else {
+    alert("acceso invalido")
+  }
+   
+    })
+
