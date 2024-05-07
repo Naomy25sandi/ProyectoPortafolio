cc78a6e4f89556cc2a1904e68a28b4def7b071fc
Naomy25sandi <naomysandi25@gmail.com>
2024-05-07 13:28:01 -0600

    elimine un estilo en la carpeta de registro

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,11 +4,4 @@ const password = document.getElementById("password")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let regexEmail =
-    if ( nombre.value.length <6) {
-        alert("Nombre muy corto")
-    }
-})
+
