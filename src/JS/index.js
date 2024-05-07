5a3b649f99965bff58d9ebbf48788f0fa18aba85
Naomy25sandi <naomysandi25@gmail.com>
2024-05-07 13:23:54 -0600

    cambie nombres de mis archivos para organizar mi trabajo

---
+++ src/JS/index.js
@@ -0,0 +1,14 @@
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let regexEmail =
+    if ( nombre.value.length <6) {
+        alert("Nombre muy corto")
+    }
+})
