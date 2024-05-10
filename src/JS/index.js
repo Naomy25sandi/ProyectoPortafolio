34db1d1f39cf23dc875e006169db1463f03eaef9
Naomy25sandi <naomysandi25@gmail.com>
2024-05-10 13:08:52 -0600

    cambios

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,23 +1,39 @@
-//Registro
-
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
-const btnRegistrarse = document.getElementById("btnRegistrarse")
-
-function guardar_localstorage() {
-    //acceso a mas usuarios con una lista//
-    usuario = [nombre.value, email.value, password.value]
-
-    localStorage.setItem("usuario",usuario)
-    
+const form = document.querySelector("form")
+form.addEventListener('submit',(e)=>){
+    e.preventDefault()
+    window.location.href= "login.html"
 }
 
-btnRegistrarse.addEventListener("click",()=>{   
+
+
+
+
+//Registro
+
+//const nombre = document.getElementById("name")
+//const email = document.getElementById("email")
+//const password = document.getElementById("password")
+//const form = document.getElementById("form")
+//const parrafo = document.getElementById("warnings")
+//const btnRegistrarse = document.getElementById("btnRegistrarse")
+
+//let listaEmail =[]
+
+//localStorage.getItem("listaEmail")
+
+
+
+//function guardar_localstorage() {
+    //acceso a mas usuarios con una lista//
+   // usuario = [nombre.value, email.value, password.value]
+
+   // localStorage.setItem("usuario",usuario)
+    
+//}
+
+//btnRegistrarse.addEventListener("click",()=>{   
     //alert("BOTON SUSCRIBIRSE")
-    guardar_localstorage()
-})
+   // guardar_localstorage()
+//})
 
 
