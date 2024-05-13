16386d67cc4d78747f871531b8a2a5d210a1c807
Naomy25sandi <naomysandi25@gmail.com>
2024-05-13 09:44:45 -0600

    registro funcionando

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,39 +1,19 @@
-// const nombre = document.getElementById("name")
-// const email= document.getElementById("email")
-// const password = document.getElementById("password")
-// const form = document.getElementById("form")
-// const btnRegistrarse = document.getElementById("btnRegistrarse")
-
-// let usuarios = [] // nuevos usuarios//
-// // crear funcion para acceso a mas usuarios con una lista//
-// function guardar_localStorage() {
-//    if (localStorage.getItem("usuarios")) {
-//     usuarios=JSON.parse(localStorage.getItem("usuarios"));
-//     }
-//  else{
-//     usuarios=[]
-//    }  
-// }
-//const nuevoUsuario=(nombre= nombre.value,email= email.value,password= password.value);
-//usuarios.push(nuevoUsuario);
-//localStorage.setItem("usuarios",JSON.stringify(usuarios));
-//alert("Usuario Registrado");
-//window.location.href="login.html"
-
+const arregloUsuarios = JSON.parse(localStorage.getItem("registrados")) || [];
+const nombre = document.getElementById("name")
+const email= document.getElementById("email")
+const password = document.getElementById("password")
 
 btnRegistrarse.addEventListener("click",()=>{
-    const nombre = document.getElementById("name").value
-    const email= document.getElementById("email").value
-    const password = document.getElementById("password").value
-    const btnRegistrarse = document.getElementById("btnRegistrarse").value
     let usuarios = {
-        nombre : nombre,
-        email : email,
-        password : password,
-    }
-    localStorage.setItem("usuario", JSON.stringify(usuarios))
+        nombre: nombre.value,
+        email: email.value,
+        password: password.value,
+    }   
+    arregloUsuarios.push(usuarios)
+
+    localStorage.setItem("registrados", JSON.stringify(arregloUsuarios))
     alert("Registro existoso");
-    window.Location.href="../login.html"
+    window.location.href="login.html"
 });
 
 
--- src/JS/login.js
+++ src/JS/login.js
@@ -1,36 +1,20 @@
-//Login
 const email = document.getElementById("email")
 const password = document.getElementById("password")
-const btn = document.getElementById("btn")
+const btnIngresar = document.getElementById("btn")
 
-btn.addEventListener("click",(e)=>{
-  e.preventDefault;
-})
-//Obtenemos los datos y contrase'a ingresados por el usuario
-const emailIngresado = email.value;
-const passwordIngresado = password.value;
-
-//obtenemos la info local storage
-const perfiles= JSON.parse(localStorage.getItem("usuarios"))||[];
-
-let perfilEncontrado = false;
-//para saber si encontro el perfil
-// Recorremos los perfiles que son ingresados para buscar el perfil ingresado por el usuario
-for (let i = 0; i < perfiles.length; i++) {
-  if (perfiles[i].email===emailIngresado && perfiles[index].password===passwordIngresado) {
- // si encuentra el perfil, me envia a portafolio//
-    Window.location.href = "portafolio.html"
-  perfilEncontrado = true;
-  break;
-  // se detiene el ciclo de busqueda//
-  }}
-  //si no encuentra perfil mostrar alerta//
-if (!perfilEncontrado) {
-  alert("acceso Invalido");
-  
+const perfiles = JSON.parse(localStorage.getItem("registrados")) || []
+function validarDatos() {
+  for (let index = 0; index < perfiles.length; index++) {
+    if (perfiles[index].email === email.value && perfiles[index].password === password.value) {
+      alert("Registro exitoso")
+      window.location.href="portafolio.html"
+      return
+    }
+  }
+  alert("DATOS INCORRECTOS ❌")
 }
-
-
-
+btnIngresar.addEventListener("click",()=>{
+  validarDatos()
+})
 
 
