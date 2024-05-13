ed7cf33b8ef45c61d4bfab6ad15f9feb1259ede4
Naomy25sandi <naomysandi25@gmail.com>
2024-05-13 09:09:19 -0600

    cambie codigo js

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,28 +1,39 @@
-const nombre = document.getElementById("name")
-const email= document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const btnRegistrarse = document.getElementById("btnRegistrarse")
+// const nombre = document.getElementById("name")
+// const email= document.getElementById("email")
+// const password = document.getElementById("password")
+// const form = document.getElementById("form")
+// const btnRegistrarse = document.getElementById("btnRegistrarse")
 
-// crear funcion para acceso a mas usuarios con una lista//
-function guardar_localStorage() {
-    let usuarios  // nuevos usuarios//
-   if (localStorage.getItem("usuarios")) {
-    usuarios=JSON.parse(localStorage.getItem("usuarios"));
-    }
- else{
-    usuarios=[]
-   }  
-}
-const nuevoUsuario=(nombre: nombre.value,email: email.value,password: password.value);
-usuarios.push(nuevoUsuario);
-localStorage.setItem("usuarios",JSON.stringify(usuarios));
-alert("Usuario Registrado");
-window.location.href="login.html"
+// let usuarios = [] // nuevos usuarios//
+// // crear funcion para acceso a mas usuarios con una lista//
+// function guardar_localStorage() {
+//    if (localStorage.getItem("usuarios")) {
+//     usuarios=JSON.parse(localStorage.getItem("usuarios"));
+//     }
+//  else{
+//     usuarios=[]
+//    }  
+// }
+//const nuevoUsuario=(nombre= nombre.value,email= email.value,password= password.value);
+//usuarios.push(nuevoUsuario);
+//localStorage.setItem("usuarios",JSON.stringify(usuarios));
+//alert("Usuario Registrado");
+//window.location.href="login.html"
 
 
 btnRegistrarse.addEventListener("click",()=>{
-    guardar_localStorage()
+    const nombre = document.getElementById("name").value
+    const email= document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const btnRegistrarse = document.getElementById("btnRegistrarse").value
+    let usuarios = {
+        nombre : nombre,
+        email : email,
+        password : password,
+    }
+    localStorage.setItem("usuario", JSON.stringify(usuarios))
+    alert("Registro existoso");
+    window.Location.href="../login.html"
 });
 
 
